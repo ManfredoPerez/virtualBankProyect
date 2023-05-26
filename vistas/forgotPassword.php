13b4aa78a3d62333b026d30da09e76062d38b72c
EdvinCanastuj <ecanastujv@miumg.edu.gt>
2023-05-26 15:31:21 -0600

    funcion para enviar correos corregida

--- vistas/forgotPassword.php
+++ vistas/forgotPassword.php
@@ -1,33 +1,97 @@
 <?php
 session_start();
-$sessData = !empty($_SESSION['sessData'])?$_SESSION['sessData']:'';
-if(!empty($sessData['status']['msg'])){
+$sessData = !empty($_SESSION['sessData']) ? $_SESSION['sessData'] : '';
+if (!empty($sessData['status']['msg'])) {
     $statusMsg = $sessData['status']['msg'];
     $statusMsgType = $sessData['status']['type'];
     unset($_SESSION['sessData']['status']);
 }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Banca Virtual</title>
     <link rel="stylesheet" href="./css/stylerespass.css" type="text/css" media="all" />
-    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900" 	type="text/css" media="all">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900" type="text/css" media="all">
 </head>
 <body>
-    <a ><h1>Restablecer Contraseña</h1></a>
-	<div class="container">
-		<h3>Introduce tu dirección de correo que usaste en el registro. Te enviaremos un correo con tu nombre de usuario y un enlace para que puedas restablecer tu contraseña.</h3>
-        <?php echo !empty($statusMsg)?'<p class="'.$statusMsgType.'">'.$statusMsg.'</p>':''; ?>
-		<div class="regisFrm">
-			<form action="./userAccount.php" method="post">
-				<input type="email" name="email" placeholder="Correo" required="">
-				<div class="send-button">
-					<input type="submit" name="forgotSubmit" value="Continuar">
-				</div>
-			</form>
-		</div>
-		<h5>Si necesitas más ayuda, contactate con el administrador.</h5>
-	</div>
+    <a><h1>Restablecer Contraseña</h1></a>
+    <div class="container">
+        <h3>Introduce tu dirección de correo que usaste en el registro. Te enviaremos un correo con tu nombre de usuario y un enlace para que puedas restablecer tu contraseña.</h3>
+        <div class="regisFrm">
+            <form action="" method="post" name="forgotForm">
+                <input type="email" name="email" placeholder="Correo" required="">
+                <div class="send-button">
+                    <input type="submit" name="forgotSubmit" value="Continuar">
+                </div>
+            </form>
+        </div>
+        <h5>Si necesitas más ayuda, contáctate con el administrador.</h5>
+    </div>
+    <?php
+    include 'user.php';
+    $user = new User();
+    if (isset($_POST['forgotSubmit'])) {
+        // Establecer la conexión a la base de datos (rellena los valores correspondientes)
+		
+        $dbHost = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbName = "bancavirtual";
+        $userTbl = "infocliente";
+
+        // Conectar a la base de datos
+        $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+        if ($conn->connect_error) {
+            die("Failed to connect with MySQL: " . $conn->connect_error);
+        }
+
+        // Obtener el correo ingresado
+        $email = $_POST['email'];
+        // Verificar si el correo existe en la tabla "infocliente"
+        $sql = "SELECT Email, Nombre FROM infocliente WHERE Email = '$email'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+           
+            $nom = $row['Nombre'];
+            $to = $row['Email'];
+            // El correo existe en la tabla
+            // Puedes realizar las acciones correspondientes aquí, como enviar el correo de restablecimiento de contraseña
+            $resetPassLink = 'http://localhost:82/virtualBankProyect/vistas/resetPassword.php?fp_code=';
+
+            // Enviar correo de restablecimiento de contraseña
+            $subject = "Solicitud de Cambio de Contraseña";
+            $mailContent = 'Estimad@ '.$nom.', 
+            <br/><br/>Recientemente se envió una solicitud para restablecer una contraseña para su cuenta. Si esto fue un error, simplemente ignore este correo electrónico y no pasará nada.
+            <br/>Para restablecer su contraseña, visite el siguiente enlace: <a href="'.$resetPassLink.'">'.$resetPassLink.'</a>
+            <br/><br/>Saludos,
+            <br/>
+            <br/>Edvin 
+            <br/>';                
+                
+            // Establecer encabezados para enviar el correo HTML
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            // Establecer encabezado adicional
+            $headers .= 'From: banckavirtual.com' . "\r\n";
+            // Enviar correo
+            
+            if (mail($to, $subject, $mailContent, $headers)) {
+                echo "Correo enviado satisfactoriamente";
+            } else {
+                echo "Error al enviar el correo";
+            }
+        } else {
+            // El correo no existe en la tabla
+            echo "Correo no encontrado en la base de datos";
+        }
+
+        // Cerrar la conexión a la base de datos
+        mysqli_close($conn);
+    }
+    ?>  
 </body>
 </html>
--- vistas/resetPassword.php
+++ vistas/resetPassword.php
@@ -1,34 +1,112 @@
 <?php
 session_start();
-$sessData = !empty($_SESSION['sessData'])?$_SESSION['sessData']:'';
-if(!empty($sessData['status']['msg'])){
+$sessData = !empty($_SESSION['sessData']) ? $_SESSION['sessData'] : '';
+if (!empty($sessData['status']['msg'])) {
     $statusMsg = $sessData['status']['msg'];
     $statusMsgType = $sessData['status']['type'];
     unset($_SESSION['sessData']['status']);
 }
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['resetSubmit'])) {
+    // Verificar si se han enviado las contraseñas
+    if (!empty($_POST['actual']) && !empty($_POST['password']) && !empty($_POST['confirm_password'])) {
+        $actualPassword = $_POST['actual'];
+        $newPassword = $_POST['password'];
+        $confirmPassword = $_POST['confirm_password'];
+
+        // Establecer la conexión a la base de datos (rellena los valores correspondientes)
+        $dbHost = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbName = "bancavirtual";
+        $userTbl = "infocliente";
+
+        // Conectar a la base de datos
+        $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+        if ($conn->connect_error) {
+            die("Failed to connect with MySQL: " . $conn->connect_error);
+        }
+
+        // Obtener el correo de la sesión
+        $email = $_SESSION['email'];
+
+        // Buscar la contraseña actual en la base de datos
+        $sql = "SELECT password FROM infocliente WHERE Email = '$email'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $hashedPassword = $row['password'];
+
+            // Verificar si la contraseña actual coincide
+            if (password_verify($actualPassword, $hashedPassword)) {
+                // Verificar si las nuevas contraseñas coinciden
+                if ($newPassword === $confirmPassword) {
+                    // Actualizar la contraseña en la tabla "infocliente"
+                    $newHashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+                    $updateSql = "UPDATE infocliente SET password = '$newHashedPassword' WHERE Email = '$email'";
+                    if ($conn->query($updateSql) === true) {
+                        $statusMsg = "Contraseña actualizada correctamente";
+                        $statusMsgType = "success";
+                    } else {
+                        $statusMsg = "Error al actualizar la contraseña";
+                        $statusMsgType = "error";
+                    }
+                } else {
+                    $statusMsg = "Las nuevas contraseñas no coinciden";
+                    $statusMsgType = "error";
+                }
+            } else {
+                $statusMsg = "La contraseña actual es incorrecta";
+                $statusMsgType = "error";
+            }
+        } else {
+            $statusMsg = "Correo no encontrado en la base de datos";
+            $statusMsgType = "error";
+        }
+
+        // Cerrar la conexión a la base de datos
+        $conn->close();
+
+        // Guardar el mensaje de estado en la sesión
+        $_SESSION['sessData']['status']['msg'] = $statusMsg;
+        $_SESSION['sessData']['status']['type'] = $statusMsgType;
+        header("Location: resetPassword.php");
+        exit();
+    } else {
+        $statusMsg = "Por favor, completa todos los campos";
+        $statusMsgType = "error";
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Banca Virtual</title>
     <link rel="stylesheet" href="./css/stylerespass.css" type="text/css" media="all" />
-    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900" 	type="text/css" media="all">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900" type="text/css" media="all">
 </head>
 <body>
     <h1>Restableciendo Contraseña</h1>
-	<div class="container">
-		<h2>Resetea la Contraseña de tu Cuenta</h2>
-        <?php echo !empty($statusMsg)?'<p class="'.$statusMsgType.'">'.$statusMsg.'</p>':''; ?>
-		<div class="regisFrm">
-			<form action="userAccount.php" method="post">
-				<input type="password" name="password" placeholder="Contraseña" required="">
-				<input type="password" name="confirm_password" placeholder="Confirma tu Contraseña" required="">
-				<div class="send-button">
-					<input type="hidden" name="fp_code" value="<?php echo $_REQUEST['fp_code']; ?>"/>
-					<input type="submit" name="resetSubmit" value="Resetea Contraseña">
-				</div>
-			</form>
-		</div>
-	</div>
+    <div class="container">
+        <h2>Resetea la Contraseña de tu Cuenta</h2>
+        <div class="regisFrm">
+            <?php
+            if (!empty($statusMsg) && !empty($statusMsgType)) {
+                echo '<div class="alert-' . $statusMsgType . '">' . $statusMsg . '</div>';
+            }
+            ?>
+            <form action="" method="post">
+                <input type="password" name="actual" placeholder="Contraseña actual" required="">
+                <h3>Escribe tu nueva contraseña</h3>
+                <input type="password" name="password" placeholder="Contraseña" required="">
+                <input type="password" name="confirm_password" placeholder="Confirma tu Contraseña" required="">
+                <div class="send-button">
+                    <input type="submit" name="resetSubmit" value="Resetea Contraseña">
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
